dcc2eb7a920c371ca593b419af4f6f29054eac3e
Teetawat Chitpitak <chitpitak.t@gmail.com>
2022-10-29 01:33:05 +0700

    update glass opacity

--- src/components/NormalCard.vue
+++ src/components/NormalCard.vue
@@ -94,7 +94,7 @@ defineProps({ cardData: Object as () => ICardNormal });
 </template>
 <style lang="scss" scoped>
 .glass {
-  background: rgba(255, 255, 255, 0.1);
+  background: rgba(255, 255, 255, 0.3);
   border-radius: 25px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(10px);
